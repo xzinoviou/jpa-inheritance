b885ec588fc75c86dac470a2527c4eb8c90b2351
Xenofon Zinoviou <xzinoviou@gmail.com>
2024-04-23 23:07:54 +0300

    add employee repository

---
+++ src/main/java/com/xzinoviou/jpainheritance/employee/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.xzinoviou.jpainheritance.employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : Xenofon Zinoviou
+ */
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
